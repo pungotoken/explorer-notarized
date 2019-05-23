ec34b8beb2009207a818e92e5b81613e11dc90fe
gcharang <mrgcharang@gmail.com>
2019-05-24 00:48:01 +0530

    makes patch script more chatty

--- patch.sh
+++ patch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cp status.html ../$1-explorer/node_modules/insight-ui-komodo/public/views/status.html
+echo "status.html copied"
 cp header.html ../$1-explorer/node_modules/insight-ui-komodo/public/views/includes/header.html
+echo "header.html copied"
 cp status.js ../$1-explorer/node_modules/insight-api-komodo/lib/status.js
+echo "status.js copied"
 cp bitcoind.js ../$1-explorer/node_modules/bitcore-node-komodo/lib/services/bitcoind.js
-
+echo "bitcoind.js copied"
