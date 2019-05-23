021559ed6abffdb571b56600259b0f633984d9ac
gcharang <mrgcharang@gmail.com>
2019-05-24 00:26:52 +0530

    modifies the patch script to work with my expllorer-insytall repo

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 
-
-cp status.html ~/explorer/$1-explorer/node_modules/insight-ui-komodo/public/views/status.html
-cp header.html ~/explorer/$1-explorer/node_modules/insight-ui-komodo/public/views/includes/header.html
-cp status.js ~/explorer/$1-explorer/node_modules/insight-api-komodo/lib/status.js
-cp bitcoind.js ~/explorer/$1-explorer/node_modules/bitcore-node-komodo/lib/services/bitcoind.js
+cp status.html ../$1-explorer/node_modules/insight-ui-komodo/public/views/status.html
+cp header.html ../$1-explorer/node_modules/insight-ui-komodo/public/views/includes/header.html
+cp status.js ../$1-explorer/node_modules/insight-api-komodo/lib/status.js
+cp bitcoind.js ../$1-explorer/node_modules/bitcore-node-komodo/lib/services/bitcoind.js
 
