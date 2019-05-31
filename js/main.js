522b13cc8e02a94a11317a1c96d4378d9a7b65ea
gcharang <mrgcharang@gmail.com>
2019-05-31 17:55:40 +0530

    updates data source to https

--- js/main.js
+++ js/main.js
@@ -44,41 +44,47 @@ angular.module('insight.currency', []);
 angular.module('insight.messages', []);
 
 // Source: public/src/js/controllers/ad.js
-angular.module("insight.ad").controller("AdController", function ($scope, $http) {
-    $http.get("http://data.lordofthechains.com/info/ecosystem.json").success(function (data, status, headers, config) {
-
+angular
+  .module("insight.ad")
+  .controller("AdController", function($scope, $http) {
+    $http
+      .get("https://data.lordofthechains.com/info/ecosystem.json")
+      .success(function(data, status, headers, config) {
         ads = {};
         priorities = {};
-        data.forEach(function (project) {
-            ads[project.projectName] = project.ads;
-            priorities[project.projectName] = project.priority;
+        data.forEach(function(project) {
+          ads[project.projectName] = project.ads;
+          priorities[project.projectName] = project.priority;
         });
         // finding the project whose ad has to be displayed now
         for (var key in priorities) {
-            if (priorities[key] == "always") {
-                var project = key;
-            } else if (priorities[key] == "never") {
-                delete priorities[key];
-            }
+          if (priorities[key] == "always") {
+            var project = key;
+          } else if (priorities[key] == "never") {
+            delete priorities[key];
+          }
         }
         if (!project) {
-            var sum = 0;
+          var sum = 0;
 
-            for (var key in priorities) {
-                sum += parseFloat(priorities[key]);
+          for (var key in priorities) {
+            sum += parseFloat(priorities[key]);
+          }
+          ranges = {};
+          rangeEnd = 1;
+          for (var key in priorities) {
+            ranges[key] = [
+              rangeEnd,
+              rangeEnd + parseFloat(priorities[key] - 1)
+            ];
+            rangeEnd = rangeEnd + parseFloat(priorities[key]);
+          }
+          rand = Math.floor(Math.random() * sum) + 1;
+          for (var key in ranges) {
+            if (rand >= ranges[key][0] && rand <= ranges[key][1]) {
+              var project = key;
             }
-            ranges = {};
-            rangeEnd = 1;
-            for (var key in priorities) {
-                ranges[key] = [rangeEnd, rangeEnd + parseFloat(priorities[key] - 1)];
-                rangeEnd = rangeEnd + parseFloat(priorities[key])
-            }
-            rand = Math.floor(Math.random() * sum) + 1
-            for (var key in ranges) {
-                if (rand >= ranges[key][0] && rand <= ranges[key][1]) {
-                    var project = key;
-                }
-            }
+          }
         }
         // finding the ad in the selected project
 
@@ -86,34 +92,37 @@ angular.module("insight.ad").controller("AdController", function ($scope, $http)
         allAds = [];
         var sum = 0;
         for (var i = 0; i < ads.length; i++) {
-            if (ads[i].frequency == "always") {
-                var toDisplay = ads[i].data
-            } else if (ads[i].frequency == "never") {
-
-            } else {
-                allAds.push(ads[i])
-            }
+          if (ads[i].frequency == "always") {
+            var toDisplay = ads[i].data;
+          } else if (ads[i].frequency == "never") {
+          } else {
+            allAds.push(ads[i]);
+          }
         }
 
         if (!toDisplay) {
-            ranges = [];
-            rangeEnd = 1;
-            for (i = 0; i < allAds.length; i++) {
-                ranges[i] = [rangeEnd, rangeEnd + parseFloat(allAds[i].frequency - 1)];
-                rangeEnd = rangeEnd + parseFloat(allAds[i].frequency)
+          ranges = [];
+          rangeEnd = 1;
+          for (i = 0; i < allAds.length; i++) {
+            ranges[i] = [
+              rangeEnd,
+              rangeEnd + parseFloat(allAds[i].frequency - 1)
+            ];
+            rangeEnd = rangeEnd + parseFloat(allAds[i].frequency);
+          }
+          rand = Math.floor(Math.random() * sum) + 1;
+          for (i = 0; i < ranges.length; i++) {
+            if (rand >= ranges[i][0] && rand <= ranges[i][1]) {
+              var toDisplay = i;
             }
-            rand = Math.floor(Math.random() * sum) + 1
-            for (i = 0; i < ranges.length; i++) {
-                if (rand >= ranges[i][0] && rand <= ranges[i][1]) {
-                    var toDisplay = i;
-                }
-            }
+          }
         }
-        $scope.toDisplay = toDisplay
-    }).error(function () {
-        $scope.error = true
-    })
-});
+        $scope.toDisplay = toDisplay;
+      })
+      .error(function() {
+        $scope.error = true;
+      });
+  });
 
 // Source: public/src/js/controllers/address.js
 angular.module('insight.address').controller('AddressController',
