0dc66d71942aecca8ba3a4de96458b9ae2f5115c
gcharang <mrgcharang@gmail.com>
2019-06-04 13:11:22 +0530

    clarity on the css situation

--- insight-ui-komodo/insight-ui-dev.sh
+++ insight-ui-komodo/insight-ui-dev.sh
@@ -3,4 +3,7 @@
 /home/$USER/.nvm/versions/node/v4.9.1/bin/npm install
 ./node_modules/.bin/bower install
 
-# run `./node_modules/.bin/grunt compile` to compile the js to minified & uglified and reflect your changes to the src in the live website. The output is at: `./public/js/` 
+# run `./node_modules/.bin/grunt compile` to compile the js to minified & uglified and reflect your changes to the src in the live website. The output is at: `./public/js/ and /public/css/`
+
+# run `./node_modules/.bin/grunt` to dev and have the changes reflect in the browser
+
