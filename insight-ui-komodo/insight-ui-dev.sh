699d608ddf0e0a3f92779aa66b7c22bab7ee89ce
gcharang <mrgcharang@gmail.com>
2019-06-15 22:11:57 +0530

    adds instructions about handling updates to css

--- insight-ui-komodo/insight-ui-dev.sh
+++ insight-ui-komodo/insight-ui-dev.sh
@@ -5,5 +5,9 @@
 
 # run `./node_modules/.bin/grunt compile` to compile the js to minified & uglified and reflect your changes to the src in the live website. The output is at: `./public/js/ and /public/css/`
 
-# run `./node_modules/.bin/grunt` to dev and have the changes reflect in the browser
+# run `./node_modules/.bin/grunt` to dev and have the changes reflect in the browser on refresh
+
+# To change css properly, change the css in ./public/src/css/common.css then compile it. The resulting main.min.css will have the necessary changes
+
+# To override specific css to reskin or change the theme of the explorer, add a file named common.min.css to the directory ./public/css/ and add all the over rides to it.
 
